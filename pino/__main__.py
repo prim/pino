929d7e40644201c390301611a46e89b796cb249e
prim <1210102@qq.com>
2018-11-21 14:57:33 +0800

    xxx

--- pino/__main__.py
+++ pino/__main__.py
@@ -3,16 +3,18 @@
 
 import sys
 
-if sys.argv[1] == "proxy":
-    import proxy
-    proxy.main()
-elif sys.argv[1] == "test":
-    import test
-    test.main()
-elif sys.argv[1] == "cli":
-    import cli
-    cli.main()
-else:
-    import server
-    server.main()
+def main():
+    if len(sys.argv) < 2:
+        print "python pino proxy"
+        print "python pino server"
+        print "python pino cli"
+        print "python pino test"
+        print "python pino worker"
+        return 
+    modname = sys.argv[1]
+    module = __import__(modname)
+    module.main()
+
+if __name__ == "__main__":
+    main()
 
